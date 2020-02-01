1b91ac89b959a85a9ab0c588b4c5f400ac1e79de
Roman <kashik.roman@rambler.ru>
2020-02-01 15:51:10 +0200

    body-size

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,14 @@ $(function () {
   //magnific-popup
   $('.popup-modal').magnificPopup({
     type: 'inline',
+    // fixedContentPos: true,
     preloader: false,
     focus: '#link-popup',
   })
 
   $('.popup-about').magnificPopup({
     type: 'inline',
+    // fixedContentPos: true,
   })
 
   $(document).on('click', '.popup-about-dismiss', function (e) {
@@ -18,6 +20,17 @@ $(function () {
     e.preventDefault()
     $.magnificPopup.close()
   })
+
+  var getIphoneWindowHeight = function() {
+// Get zoom level of mobile Safari
+// Such zoom detection might not work correctly on other platforms
+    //
+    var zoomLevel = document.documentElement.clientWidth / window.innerWidth;
+// window.innerHeight returns height of the visible area.
+    // We multiply it by zoom and get our real height.
+    return window.innerHeight * zoomLevel;};
+
+
 //pagetoscroll
   $(window).on('load', function () {
     $('a,a[href=\'#top\'],a[rel=\'m_PageScroll2id\'],a.PageScroll2id').
